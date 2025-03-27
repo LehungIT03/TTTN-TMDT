6dabdf73c984cec1503736b8efae6a4728b9629a
MSI\hung1 <hung1456320@gmail.com>
2025-03-27 19:05:39 +0700

    no message

--- thuongmaidientu/src/data/categoriesData.js
+++ thuongmaidientu/src/data/categoriesData.js
@@ -41,7 +41,10 @@ export const categories = [
     name: "Tai nghe",
     slug: "tai-nghe",
     img: "https://cdn-icons-png.flaticon.com/128/3791/3791461.png",
+<<<<<<< HEAD
 
+=======
+>>>>>>> 307a585 (no message)
   },
   {
     id: 6,
